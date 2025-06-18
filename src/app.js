9a56e53b30038372e38b8cd1d6af40b50a21c70e
sathvik32 <sathvi.thatikonda@gmail.com>
2025-06-18 14:03:19 -0500

    Final backend code for two new users

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db'); // <-- import MongoDB connector
 const authRoutes = require('./routes/authRoutes'); // <-- import routes
 const userRoutes = require('./routes/userRoutes');
+const managerRoutes = require('./routes/managerRoutes');
+const workerRoutes = require('./routes/workerRoutes');
 
 
 dotenv.config();
@@ -31,4 +33,6 @@ const PORT = process.env.PORT || 5000;
 // make sure you add .env file to .gitignore
 app.use('/api/auth', authRoutes); // ✅ enables /signup and /login routes
 app.use('/api/users', userRoutes); 
+app.use('/api/managers', managerRoutes);
+app.use('/api/workers', workerRoutes);
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -28,4 +28,13 @@ const adminOnly = (req, res, next) => {
   next();
 };
 
-module.exports = { protect, adminOnly };
+const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: 'Forbidden: Role not allowed' });
+    }
+    next();
+  };
+};
+
+module.exports = {protect, adminOnly, authorizeRoles };
